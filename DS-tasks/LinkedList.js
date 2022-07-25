4136320e3f9c69ceac70292ded2832e65d774318
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-07-25 12:25:15 +0200

    solution

--- DS-tasks/LinkedList.js
+++ DS-tasks/LinkedList.js
@@ -1,3 +1,12 @@
 /*
 Given a pointer to the head of a linked list, insert a new node before the head. The value in the new node should point to and the value should be replaced with a given value. Return a reference to the new head of the list. The head pointer given may be null meaning that the initial list is empty.
 */
+function insertNodeAtHead(head, data) {
+    
+    if(head == null){
+          return new SinglyLinkedListNode(data);
+      }
+      var node = new SinglyLinkedListNode(data);
+      node.next = head;
+      return node;
+  }
