b3ad4779e05c356dc831f6e1d210addd6973ce8b
Saidamir <69676883+Saidamir-Sh@users.noreply.github.com>
2022-07-25 11:42:25 +0200

    inserting element at the head of the list

---
+++ DS-tasks/LinkedList.js
@@ -0,0 +1,3 @@
+/*
+Given a pointer to the head of a linked list, insert a new node before the head. The value in the new node should point to and the value should be replaced with a given value. Return a reference to the new head of the list. The head pointer given may be null meaning that the initial list is empty.
+*/
