164393a0e213f555ca601bd97f75ec83bd13aa07
Saidamir-Sh <saidamirpl@gmail.com>
2023-08-15 21:58:40 +0200

    merge sorted array

--- Algorithms/Sorting/BubbleSort/BubbleSort.md
+++
@@ -1,2 +0,0 @@
-https://leetcode.com/problems/sort-colors/
-https://leetcode.com/problems/majority-element/solutions/3676530/3-method-s-beats-100-c-java-python-beginner-friendly/
--- Algorithms/Sorting/BubbleSort/LeetCode-problems.js
+++ Algorithms/Sorting/BubbleSort/LeetCode-problems.js
@@ -1,9 +1,6 @@
+// Problem : Majority Element
 // https://leetcode.com/problems/majority-element/description/
-
-/**
- * @param {number[]} nums
- * @return {number}
- */
+// https://leetcode.com/problems/majority-element/solutions/3676530/3-method-s-beats-100-c-java-python-beginner-friendly/
 var majorityElement = function(nums) {
     let len = nums.length
     let checked
@@ -19,4 +16,38 @@ var majorityElement = function(nums) {
         }
     } while(checked)
     return nums[Math.floor(nums.length / 2)]
+};
+
+// Problem : Sort Colors
+// https://leetcode.com/problems/sort-colors/
+var sortColors = function(nums) {
+    let len = nums.length;
+    let checked;
+    do {
+        checked = false
+        for(let i = 0; i < len; i++) {
+            if(nums[i] > nums[i+1]) {
+                let temp = nums[i]
+                nums[i] = nums[i + 1]
+                nums[i + 1] = temp
+                checked = true
+            }
+        }
+    } while(checked)
+    return nums
+};
+
+// Problem: Contains Duplicate
+// https://leetcode.com/problems/contains-duplicate/description/
+var containsDuplicate = function(nums) {
+    const set = new Set(nums)
+    return set.size !== nums.length
+ };
+
+//  https://leetcode.com/problems/merge-sorted-array/description/
+var merge = function(nums1, m, nums2, n) {
+    for(let j = m, i = 0; i < n; j++, i++) {
+        nums1[j] = nums2[i]
+    }  
+    nums1.sort((a, b) => a-b)
 };
