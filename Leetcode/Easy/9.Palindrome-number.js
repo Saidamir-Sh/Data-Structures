eed971be099521d30798857c7337bffa10677f19
Saidamir-Sh <saidamirpl@gmail.com>
2023-10-12 22:49:11 +0200

    leetcode problem: 1, 9, 13, 14

---
+++ Leetcode/Easy/9.Palindrome-number.js
@@ -0,0 +1,9 @@
+// Topic: [Math]
+
+// Given an integer x, return true if x is a palindrome, and false otherwise
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
