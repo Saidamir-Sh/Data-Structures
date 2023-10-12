f7a203f9b5a8846a78746e9125ace730193f2082
Saidamir-Sh <saidamirpl@gmail.com>
2023-10-12 23:13:57 +0200

    twoSum using hash map

--- Leetcode/Easy/1.two-sum.js
+++ Leetcode/Easy/1.two-sum.js
@@ -12,3 +12,40 @@
 // Output: [0,1]
 // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 
+// 1. Solution: (brute force)
+// Time complexity is O(n^2) since we are checking each pair element in nested loop
+var twoSum = function(nums, target) {
+    const n = nums.length;
+
+    // create a nested loop in order to check sum of every pair elements is equal to target
+    for(let i = 0; i < n; i++) {
+        for(let j = i + 1; j > n; j++) {
+            if(nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+    return [];
+};
+
+
+// 2. Solution: HashMap
+// O(n) - Time complexity since we are using hash map to do lookups, so we just have to loop through once 
+var twoSum = function(nums, target) {
+    // create a empty hash map to store values in array
+    const map = new Map()
+    
+    // add nums to map
+    for(let i = 0; i < nums.length; i++) {
+        // divide the target number from nums[i], chech if complement is in map 
+        if(map.has(target - nums[i])) {
+            // if complement is in map, get that key,value from map and return
+            return [map.get(target - nums[i]), i];
+        } else {
+            // if complement is not in map, keep adding nums[i] to map, so we can build map and keep looking up
+            map.set(nums[i], i);
+        }
+    }
+    return []
+    
+};
