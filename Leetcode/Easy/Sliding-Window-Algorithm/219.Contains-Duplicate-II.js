90c97a87b74ead28db799916b6deffc683ddafb0
Saidamir-Sh <saidamirpl@gmail.com>
2023-10-28 20:45:04 +0200

    contains neearly duplicate

--- Leetcode/Easy/Sliding-Window-Algorithm/219.Contains-Duplicate-II.js
+++ Leetcode/Easy/Sliding-Window-Algorithm/219.Contains-Duplicate-II.js
@@ -18,6 +18,10 @@
 // -109 <= nums[i] <= 109
 // 0 <= k <= 105
 
+// Notes:
+// We have to return true if there is 2 similar elements in the array and the difference between their indices is less than 'k'
+// We have to store elements in the array with their indices to check if there is such case
+
 // Solution:
 /**
  * @param {number[]} nums
@@ -25,5 +29,16 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
+    let map = new Map();
     
+    // Traverse the array
+    for(let i = 0; i < nums.length; i++) {
+        // we dont need to check if nums[i] == nums[j] because current element (nums[i]) is already in the map and we are just checking if their differences between indices is less then k, if it is not in the map, map.get() returns undefined, and we set the element to map with its index as value
+        if(i - map.get(nums[i]) <= k) {
+            return true
+        } else {
+            // else add the element in map with its index
+            map.set(nums[i], i)
+        }
+    }
 };
